e11c2e90f469fbdca1d89e4fc66bf6cdeaa74b9b
saranatour1 <saranatourcontact@gmail.com>
2025-09-01 14:26:50 +0200

    chore: add basic convex-auth + update convex to latest

--- apps/web/features/LoginForm/index.tsx
+++ apps/web/features/LoginForm/index.tsx
@@ -1,24 +1,21 @@
-import { cn } from "@repo/ui/lib/utils"
+'use client'
 import { Button } from "@repo/ui/components/ui/button"
-import { Input } from "@repo/ui/components/ui/input"
-import { Label } from "@repo/ui/components/ui/label"
 import React from "react"
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"form">) {
-  return (<Button variant="secondary" className="w-full max-w-">
-    <GithubSVG/> 
-    Login with GitHub
+  return (<Button variant="secondary" className="w-full max-w-sm flex items-center justify-center" onClick={()=> console.log("Hi")}>
+    <ConvexSVG className="size-8"/>
+    <span className="text-secondary-foreground">Sign up with Convex</span>
   </Button>)
 }
 
-export const GithubSVG = ({...props}:React.ComponentProps<"svg">)=>{
-  return (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-4 h-4" {...props}>
-      <path
-        d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
-        fill="currentColor"
-      />
-    </svg>)
+export const ConvexSVG = ({ ...props }: React.ComponentProps<"svg">) => {
+  return (<svg width="184" height="188" viewBox="0 0 184 188" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M108.092 130.021C126.258 128.003 143.385 118.323 152.815 102.167C148.349 142.128 104.653 167.385 68.9858 151.878C65.6992 150.453 62.8702 148.082 60.9288 145.034C52.9134 132.448 50.2786 116.433 54.0644 101.899C64.881 120.567 86.8748 132.01 108.092 130.021Z" fill="#F3B01C" />
+    <path d="M53.4012 90.1735C46.0375 107.191 45.7186 127.114 54.7463 143.51C22.9759 119.608 23.3226 68.4578 54.358 44.7949C57.2286 42.6078 60.64 41.3097 64.2178 41.1121C78.9312 40.336 93.8804 46.0225 104.364 56.6193C83.0637 56.831 62.318 70.4756 53.4012 90.1735Z" fill="#8D2676" />
+    <path d="M114.637 61.8552C103.89 46.8701 87.0686 36.6684 68.6387 36.358C104.264 20.1876 148.085 46.4045 152.856 85.1654C153.3 88.7635 152.717 92.4322 151.122 95.6775C144.466 109.195 132.124 119.679 117.702 123.559C128.269 103.96 126.965 80.0151 114.637 61.8552Z" fill="#EE342F" />
+  </svg>)
 }
